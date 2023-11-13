a134400ee83a8c5027dc5a169ff71171630698a0
Mark McFate <summitt.dweller@gmail.com>
2023-11-13 17:41:09 -0600

    Logs CSV created

--- gch8m5.py
+++ gch8m5.py
@@ -3,6 +3,7 @@
 
 import os
 import time
+import csv 
 import json
 from pprint import pprint
 from datetime import datetime, timedelta
@@ -34,20 +35,38 @@ cache = geocaching.get_cache("GCH8M5")
 print(cache.name)      # cache.load( ) is automatically called
 print(cache.location)  # stored in cache, printed immediately
 
-#--------------------------------------------------------------------------
-# Open a logs.json file and write log data to it...
-with open('logs.json', 'w') as json_file: 
+#---------------------------------------------------------------------------
+# Read the data and write to a .csv file
 
-  #------------------------------------------------------------------------
-  # Get the log entries...
+field_names = ['_uuid', '_author', '_visited', '_type', 'membership', 'log_image', '_text'] 
+
+with open('gch8m5_logs.csv', 'w') as csvfile: 
+  writer = csv.DictWriter(csvfile, fieldnames = field_names) 
+  writer.writeheader( ) 
+	# writer.writerows(cars) 
+
   for log in cache.load_logbook(limit=2000):
     obj_vars = vars(log)
-    pprint(obj_vars)
-    # print(log.visited, log.type, log.author, log.text)
-    # print(log.uuid)
+    writer.writerow(obj_vars)
+
+
+
+
+
+# #--------------------------------------------------------------------------
+# # Open a logs.json file and write log data to it...
+# with open('gch8m5_logs.json', 'w') as json_file: 
+
+#   #------------------------------------------------------------------------
+#   # Get the log entries...
+#   for log in cache.load_logbook(limit=2000):
+#     obj_vars = vars(log)
+#     pprint(obj_vars)
+#     # print(log.visited, log.type, log.author, log.text)
+#     # print(log.uuid)
     
-    #----------------------------------------------------------------------
-    # Dump the log details as json
-    json_file.write(str(obj_vars))
-    json_file.write("\n")
-    # json_file.write(json.dumps(obj_vars))
+#     #----------------------------------------------------------------------
+#     # Dump the log details as json
+#     json_file.write(str(obj_vars))
+#     json_file.write("\n")
+#     # json_file.write(json.dumps(obj_vars))
